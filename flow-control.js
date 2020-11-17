ed7e3ec1b0f8c60ad614bb976a8fb029c17aeedc
Adam B <adamisbarker@hotmail.com>
2020-11-17 00:30:43 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,7 +5,7 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  if (age > 13 && age < 19){
+  if (age >= 13 && age <= 19){
     return ("You are a teenager!")
   } else {
       return ("You are not a teenager")
@@ -13,7 +13,7 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if (age > 13 && age < 19){
+  if (age >= 13 && age <= 19){
     return ("You are a teenager!")
   } else if (age <= 12){
     return ("You are a kid")
